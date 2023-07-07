2f496207a37093c6aa676d4fd75035f9cca3d1d5
Priyanshu Banerjee <priyanshu.ban2003@gmail.com>
2023-07-07 21:14:55 +0530

    Doubly Linked List with Addition at the End of Node

---
+++ DLL(Addition at End).c
@@ -0,0 +1,83 @@
+// Create a DLL by inserting a node at the end of the list
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct stud
+{
+	struct stud *lptr;
+	int info;
+	struct stud *rptr;
+}Node;
+Node *start=NULL,*last=NULL;
+
+//Insertion at the End function
+void add_end(int value)
+{
+Node *newptr=NULL,*tptr=NULL;
+newptr=(Node*)malloc(sizeof(Node));
+newptr->info=value;
+newptr->lptr=NULL;
+newptr->rptr=NULL;
+if(start==NULL)
+{
+start=newptr;
+}
+else
+{
+tptr=start;
+while(tptr->rptr!=NULL)
+{
+tptr=tptr->rptr;
+}
+tptr->rptr=newptr;
+newptr->lptr=tptr;
+last=newptr;
+}
+}
+// Display forward move
+void display_forward()
+{
+Node *tptr=start;
+printf("\nDATA forward:\n");
+while(tptr)
+{
+printf("->%d",tptr->info);
+tptr=tptr->rptr;
+}
+}
+// Display Backward move
+void display_backward()
+{
+Node *tptr=last;
+printf("\nDATA backward:\n");
+while(tptr)
+{
+printf("->%d",tptr->info);
+tptr=tptr->lptr;
+}
+}
+// main function
+int main()
+{
+char choice;
+int value,i,pos;
+while(1)
+{
+  	printf("\nDo you want to insert(y/n)?");
+  	scanf("%c",&choice);
+  	if(choice == 'y'||choice == 'Y')
+	{
+  	printf("\nEnter the info: ");
+  	scanf("%d",&value);
+	add_end(value);
+  	fflush(stdin);
+	}
+	else
+	{
+	break;
+	}
+}
+
+display_forward();
+display_backward();
+return 0;
+}
